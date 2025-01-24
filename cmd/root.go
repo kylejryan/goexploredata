d438edb5bd2a78f68483c5df1eaa2ae58f1626ab
Kyle <kyleryan730@gmail.com>
2025-01-23 23:42:33 -0500

    more

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,25 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "godataframe",
+	Short: "A CLI tool for dataframe operations in Go",
+	Long:  `GoDataFrame allows you to convert files into dataframes and perform statistical analyses directly from the command line.`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Define flags here
+}
--- main.go
+++ main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/kylejryan/go-data-explorer/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
